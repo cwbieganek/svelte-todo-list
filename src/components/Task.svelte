90f9f5e7fdbd4febe670f43c85e7a539ac3ce0bf
Chris <cwbieganek@gmail.com>
2021-12-31 17:15:06 -0600

    Add Task component (#1)
    * Add Task component
    
    * Move task styles to Task component
    
    * Fix Task component
    
    * Final touches on Task component

---
+++ src/components/Task.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+	export let task: Task;
+
+	interface Task {
+		id: number;
+		description: string;
+		priority: 'low' | 'medium' | 'high';
+		complete: boolean;
+	}
+
+	function handleTaskDeleteClick(e: MouseEvent) {
+		dispatch('delete', task.id);
+	}
+</script>
+
+<div class="task">
+	<input type="checkbox" class="task-complete-checkbox" />
+	<span class="task-text">{task.description}</span>
+	<button class="task-delete" on:click={handleTaskDeleteClick}>x</button>
+</div>
+
+<style>
+	.task {
+		position: relative;
+		text-align: left;
+		padding: 10px;
+		border-left: 2px solid #f7a58a;
+		border-radius: 3px;
+		background-color: #EEEEEE;
+		margin-bottom: 8px;
+	}
+
+	.task:hover {
+		border-left: 4px solid #FF3E00;
+		padding-left: 8px;
+	}
+
+	.task-delete {
+		position: absolute;
+		top: 4px;
+		right: 4px;
+		border: none;
+		background: none;
+		padding: 0;
+		line-height: 0.5em;
+	}
+
+	.task-delete:hover {
+		cursor: pointer;
+		color: red;
+	}
+</style>
--- src/components/ToDo.svelte
+++ src/components/ToDo.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Task from './Task.svelte';
+
 	export let name: string = "Someone";
 	export let taskDescriptions: string[] = [];
 	
@@ -11,6 +13,7 @@
 
 	let tasks: Task[] = [];
 	let newTask: string = "";
+	$: nextTaskId = (tasks.length == 0) ? 1 : tasks[tasks.length - 1].id + 1;
 
 	// Dynamic variables
 	$: completedTasks = tasks.filter((task) => {
@@ -36,7 +39,7 @@
 		console.log(`Adding a task: ${description}.`);
 
 		let newTask: Task = {
-			id: tasks.length + 1,
+			id: nextTaskId,
 			description: description,
 			priority: 'low',
 			complete: false
@@ -45,7 +48,8 @@
 		tasks = [...tasks, newTask];
 	}
 
-	function removeTask(id: number): void {
+	function removeTask(e: CustomEvent): void {
+		let id: number = e.detail;
 		console.log(`Removing task number ${id}.`);
 		tasks = tasks.filter((task) => {
 			return task.id !== id;
@@ -62,12 +66,6 @@
 			addTask(newTask);
 		}
 	}
-
-	function handleTaskDeleteClick(e: MouseEvent) {
-		let target = e.target as HTMLButtonElement;
-		let taskId = +target.getAttribute('data-task-id');
-		removeTask(taskId);
-	}
 </script>
 
 <div>
@@ -77,12 +75,8 @@
 			<input class="add-task-input" placeholder="Add a task" bind:value={newTask} on:keypress={handleAddTaskInputKeystroke} />
 			<button class="add-task-button" disabled={newTask.replaceAll(" ", "") === ""} on:click={(e) => {addTask(newTask);}}>Add Task</button>
 		</div>
-		{#each incompleteTasks as task}
-			<div class="task" data-task-id={task.id}>
-				<input type="checkbox" class="task-complete-checkbox" />
-				<span class="task-text">Task #{task.id}: {task.description}</span>
-				<button class="task-delete" on:click={handleTaskDeleteClick} data-task-id={task.id}>x</button>
-			</div>
+		{#each incompleteTasks as task (task.id)}
+			<Task {task} on:delete={removeTask} />
 		{/each}
 		<div class="tasks-bottom-buttons-container">
 			<button class="remove-all-tasks-button" on:click={removeAllTasks}>Remove All Tasks</button>
@@ -116,35 +110,5 @@
 
 	.add-task-button {
 		flex: 1;
-	}
-
-	.task {
-		position: relative;
-		text-align: left;
-		padding: 10px;
-		border-left: 2px solid #f7a58a;
-		border-radius: 3px;
-		background-color: #EEEEEE;
-		margin-bottom: 8px;
-	}
-
-	.task:hover {
-		border-left: 4px solid #FF3E00;
-		padding-left: 8px;
-	}
-
-	.task-delete {
-		position: absolute;
-		top: 4px;
-		right: 4px;
-		border: none;
-		background: none;
-		padding: 0;
-		line-height: 0.5em;
-	}
-
-	.task-delete:hover {
-		cursor: pointer;
-		color: red;
 	}
 </style>
