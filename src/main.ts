4d04769306c4eed77ac6f22faf68e803e39d3214
Chris Bieganek <cwbieganek@gmail.com>
2021-12-31 11:15:24 -0600

    Pass in array of tasks to App component

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		name: 'Chris',
+		tasks: [
+			'Buy milk',
+			'Schedule dentist appointment',
+			'Meal prep',
+			'Walk dog',
+			'Take over the world'
+		]
 	}
 });
 
