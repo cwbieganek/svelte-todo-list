802f8a223d1e00bef0ab6ee0ec3c15170d05691c
Chris Bieganek <cwbieganek@gmail.com>
2021-12-31 15:40:35 -0600

    Add Task component

---
+++ src/components/Task.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let task: Task;
+
+	interface Task {
+		id: number;
+		description: string;
+		priority: 'low' | 'medium' | 'high';
+		complete: boolean;
+	}
+
+	function removeTask(): void {
+		console.log('Removing this task...');
+	}
+
+	function handleTaskDeleteClick(e: MouseEvent) {
+		removeTask();
+	}
+</script>
+
+<div class="task">
+	<input type="checkbox" class="task-complete-checkbox" />
+	<span class="task-text">Task #{task.id}: {task.description}</span>
+	<button class="task-delete" on:click={handleTaskDeleteClick}>x</button>
+</div>
+
+<style>
+
+</style>
