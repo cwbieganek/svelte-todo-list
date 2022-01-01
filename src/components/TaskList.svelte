65901f9d2baddbbc7933007869b96b24bfe55c0f
Chris Bieganek <cwbieganek@gmail.com>
2021-12-31 20:03:43 -0600

    Move list of tasks to TasksList component

---
+++ src/components/TaskList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Task from './Task.svelte';
+	import type ITask from './ITask';
+	import { createEventDispatcher } from 'svelte';
+
+	export let tasks: ITask[];
+
+	const dispatch = createEventDispatcher();
+</script>
+
+{#each tasks as task (task.id)}
+	<Task {task} on:delete={(e) => {dispatch('delete', e.detail)}} />
+{/each}
+
+<style></style>
