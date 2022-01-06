55f3c26f7bfcef2ad0e5c62061da40271d483e94
Chris Bieganek <cwbieganek@gmail.com>
2022-01-05 20:48:58 -0600

    Invert background and foreground colors

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,13 +2,14 @@
 	export let text: string;
 </script>
 
-<h2 class="color-primary-dark">{text}</h2>
+<h2 class="bg-primary-dark">{text}</h2>
 
 <style>
 	h2 {
 		text-transform: uppercase;
 		font-weight: 200;
 		font-size: 3em;
+		color: white;
 		/* Box-shadow based on: https://stackoverflow.com/questions/4561097/css-box-shadow-bottom-only */
 		box-shadow: 0 4px 3px -2px #BDBDBD;
 	}
